d7365ae8cda491592aecd4b5e284a021b80e93b3
sbenjamin51 <benjamins@xavier.edu>
2024-10-07 20:44:10 -0400

    Initial Commit

---
+++ Pokehub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokehub
+//
+//  Created by Simon Benjamin on 10/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokehub/PokehubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokehubApp.swift
+//  Pokehub
+//
+//  Created by Simon Benjamin on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PokehubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
